8e32a07e0b6443797875cd47a5f3a63cf4235609
Matt Bishop <cmbishop@pdx.edu>
2016-05-06 18:45:51 -0700

    Modified app.js to reflect new restful routes, database, etc

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,36 +1,87 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
+    
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-        {name: "Crater Lake National Park", image: "http://www.parkcamper.com/Crater-Lake-National-Park/crater-lake-national-park.jpg"},    
-        {name: "Timothy Lake", image: "https://upload.wikimedia.org/wikipedia/commons/2/2d/Timothy_Lake_(Clackamas_County,_Oregon_scenic_images)_(clacD0028).jpg"},
-        {name: "Fort Stevens", image: "http://www.naturallyamazing.com/americasparks/2057.jpg"}
-];
+//schema setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
 
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//     {
+//         name: "Granite Hill", 
+//         image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg",
+//         description: "This is a huge granite hill. No bathrooms or water."
+        
+//     }, function(err, campground){
+//         if(err){
+//             console.log(err);
+//         } else {
+//             console.log("Newly created campground: ");
+//             console.log(campground);
+//         }
+//     });
+    
 app.get("/", function(req, res){
     res.render("landing");
 });
 
-app.get("/campgrounds", function(req, res) {
-    res.render("campgrounds",{campgrounds: campgrounds});
+// Index - list all campgrounds
+app.get("/campgrounds", function(req, res){
+    // Get all campgrounds from DB
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("index",{campgrounds:allCampgrounds});
+        }
+    });
 });
 
-app.get("/campgrounds/new", function(req, res) {
+// Create - add new campground to DB
+app.post("/campgrounds", function(req, res){
+    // get data from form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description: desc};
+    // create a new campground and save to DB
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
+});
+
+// New - show form to create new campground
+app.get("/campgrounds/new", function(req, res){
    res.render("new.ejs"); 
 });
 
-app.post("/campgrounds", function(req, res){
-   var name = req.body.name;
-   var image = req.body.image;
-   var newCampground = {name: name, image: image};
-   campgrounds.push(newCampground);
-   res.redirect("/campgrounds");
+// Show - shows info about selected campsite
+app.get("/campgrounds/:id", function(req, res){
+    //find the campground with provided ID
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else {
+            // render show template with that campground
+            res.render("show", {campground: foundCampground});
+        }
+    });
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("The YelpCamp server has started.");
+   console.log("The YelpCamp Server Has Started!");
 });
