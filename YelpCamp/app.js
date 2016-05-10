d20fafeab3ff33b937d2a562b9ea201b519376b4
Matt Bishop <cmbishop@pdx.edu>
2016-05-09 22:16:39 -0700

    Adding authentication

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -2,13 +2,30 @@ var express =       require("express"),
     app =           express(),
     bodyParser =    require("body-parser"),
     mongoose =      require("mongoose"),
+    passport =      require("passport"),
+    LocalStrategy = require("passport-local"),
     Campground =    require("./models/campground"),
+    Comment =       require("./models/comment"),
+    User =          require("./models/user"),
     seedDB =        require("./seeds");
     
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 seedDB(); 
+
+//PASSPORT CONFIG
+app.use(require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
     res.render("landing");
@@ -21,7 +38,7 @@ app.get("/campgrounds", function(req, res){
         if(err){
             console.log(err);
         } else {
-            res.render("index",{campgrounds:allCampgrounds});
+            res.render("campgrounds/index",{campgrounds:allCampgrounds});
         }
     });
 });
@@ -45,7 +62,7 @@ app.post("/campgrounds", function(req, res){
 
 // New - show form to create new campground
 app.get("/campgrounds/new", function(req, res){
-   res.render("new.ejs"); 
+   res.render("campgrounds/new"); 
 });
 
 // Show - shows info about selected campsite
@@ -57,8 +74,70 @@ app.get("/campgrounds/:id", function(req, res){
         } else {
             console.log(foundCampground);
             // render show template with that campground
-            res.render("show", {campground: foundCampground});
+            res.render("campgrounds/show", {campground: foundCampground});
         }
+    });
+});
+
+//=================================
+//COMMENTS ROUTES
+//=================================
+
+app.get("/campgrounds/:id/comments/new", function(req, res) {
+    //find campground by id
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("comments/new", {campground: campground});
+        }
+    });
+});
+
+app.post("/campgrounds/:id/comments/", function(req, res) {
+    //lookup campground using ID
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err);
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect('/campgrounds/' + campground._id);
+                    
+                }
+            });
+        }
+    });
+    //create new account
+    //connect new comment
+    //connect new comment to campground
+    //redirect campground show page
+});
+//===================
+//AUTH ROUTES
+//===================
+
+//show register form
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+//handle sign up logic
+app.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/campgrounds");
+        });
     });
 });
 
--- YelpCamp/seeds.js
+++ YelpCamp/seeds.js
@@ -6,17 +6,17 @@ var data = [
     {
         name: "Cloud's Rest", 
         image: "http://img1.sunset.timeinc.net/sites/default/files/image/2010/05/hoodview-campground-0510-x.jpg",    
-        description: "blah blah blah"
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ultricies magna id odio aliquam aliquet non sed purus. Donec vel tortor sodales, rutrum est nec, tincidunt lorem. Aliquam sodales mi bibendum risus vulputate, sit amet blandit ante imperdiet. Suspendisse mi mi, dignissim nec turpis placerat, molestie imperdiet massa. Donec tempus odio nec elit porttitor elementum. Morbi volutpat aliquet dolor, id porttitor est ultricies vitae. Vivamus posuere dolor sit amet ultrices euismod. Sed venenatis tortor eu egestas porta. Praesent convallis mi nibh. Integer semper nisl lacus, non venenatis lacus tempor sed. Sed ut ante quam. Suspendisse potenti. Sed tempus pretium laoreet. Morbi sit amet erat lacus. Maecenas ultricies blandit dui a porttitor."
     },
     {
         name: "Lost River", 
         image: "http://lostriver.com/images/swf/image6.jpg",    
-        description: "blah blah blah"
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ultricies magna id odio aliquam aliquet non sed purus. Donec vel tortor sodales, rutrum est nec, tincidunt lorem. Aliquam sodales mi bibendum risus vulputate, sit amet blandit ante imperdiet. Suspendisse mi mi, dignissim nec turpis placerat, molestie imperdiet massa. Donec tempus odio nec elit porttitor elementum. Morbi volutpat aliquet dolor, id porttitor est ultricies vitae. Vivamus posuere dolor sit amet ultrices euismod. Sed venenatis tortor eu egestas porta. Praesent convallis mi nibh. Integer semper nisl lacus, non venenatis lacus tempor sed. Sed ut ante quam. Suspendisse potenti. Sed tempus pretium laoreet. Morbi sit amet erat lacus. Maecenas ultricies blandit dui a porttitor."
     },
     {
         name: "Boise National Forest", 
         image: "http://www.fs.usda.gov/Internet/FSE_MEDIA/stelprdb5043024.jpg",    
-        description: "blah blah blah"
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ultricies magna id odio aliquam aliquet non sed purus. Donec vel tortor sodales, rutrum est nec, tincidunt lorem. Aliquam sodales mi bibendum risus vulputate, sit amet blandit ante imperdiet. Suspendisse mi mi, dignissim nec turpis placerat, molestie imperdiet massa. Donec tempus odio nec elit porttitor elementum. Morbi volutpat aliquet dolor, id porttitor est ultricies vitae. Vivamus posuere dolor sit amet ultrices euismod. Sed venenatis tortor eu egestas porta. Praesent convallis mi nibh. Integer semper nisl lacus, non venenatis lacus tempor sed. Sed ut ante quam. Suspendisse potenti. Sed tempus pretium laoreet. Morbi sit amet erat lacus. Maecenas ultricies blandit dui a porttitor."
     },
 ];
 
